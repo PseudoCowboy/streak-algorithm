3a14b58d00563e67a00d3f12c6151a9bee52a682
pseudocowboy <rightjiang@hotmail.com>
2021-05-10 18:57:51 +0800

    20210510

--- 2021/05/08/322CoinChange/main.go
+++ 2021/05/08/322CoinChange/main.go
@@ -25,3 +25,22 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+func coinChange1(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+	for i := 1; i < amount+1; i++ {
+		dp[i] = amount + 1
+	}
+	for i := 1; i <= amount+1; i++ {
+		for j := range coins {
+			if coins[i] < i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
