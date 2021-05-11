62a3ec7f3f3c390a812e8a290262beae63eb1710
pseudocowboy <rightjiang@hotmail.com>
2021-05-11 22:12:50 +0800

    20210511

---
+++ 2021/05/10/260SingleNumberIII/260_single_number_III.md
@@ -0,0 +1,16 @@
+# [260. Single Number III](https://leetcode.com/problems/single-number-iii/)
+
+
+## 题目
+
+Given an array of numbers `nums`, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+**Example:**
+
+    Input:  [1,2,1,3,2,5]
+    Output: [3,5]
+
+**Note**:
+
+1. The order of the result is not important. So in the above example, `[5, 3]` is also correct.
+2. Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
---
+++ 2021/05/10/64MinimumPathSum/main.go
@@ -0,0 +1,31 @@
+package main
+
+func minPathSum(grid [][]int) int {
+	dp := make([][]int, len(grid))
+	for i := range dp {
+		dp[i] = make([]int, len(grid[0]))
+	}
+
+	dp[0][0] = grid[0][0]
+	for i := 1; i < len(grid); i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for i := 1; i < len(grid[0]); i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+
+	for i := 1; i < len(grid); i++ {
+		for j := 1; j < len(grid[0]); j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+
+	return dp[len(grid)][len(grid[0])]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
