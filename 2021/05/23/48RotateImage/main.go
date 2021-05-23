a5c5106e620016d25c9084d81dc5a34077b292ef
pseudocowboy <rightjiang@hotmail.com>
2021-05-23 22:02:44 +0800

    20210523

---
+++ 2021/05/23/48RotateImage/main.go
@@ -0,0 +1,15 @@
+package main
+
+func rotate(matrix [][]int) {
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix)/2; j++ {
+			matrix[i][j], matrix[i][len(matrix)-1-j] = matrix[i][len(matrix)-1-j], matrix[i][j]
+		}
+	}
+}
