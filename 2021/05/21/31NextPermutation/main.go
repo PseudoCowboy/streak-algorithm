a95a42171b95bb9b24498881e1de604fe540e715
pseudocowboy <rightjiang@hotmail.com>
2021-05-26 02:05:31 +0800

    20210525

--- 2021/05/21/31NextPermutation/main.go
+++ 2021/05/21/31NextPermutation/main.go
@@ -1,5 +1,43 @@
 package main
 
-func nextPermutation(nums []int) {
+import "fmt"
 
+func main() {
+	arr := []int{4, 7, 5, 3, 2, 1}
+	i := 0
+	for i = len(arr) - 2; i >= 0; i-- {
+		if arr[i] < arr[i+1] {
+			break
+		}
+	}
+	fmt.Println(i)
+}
+
+func nextPermutation(nums []int) {
+	i := 0
+	for i = len(nums) - 2; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
+			break
+		}
+	}
+	if i < 0 {
+		reverse(nums)
+		return
+	}
+	for j := len(nums) - 1; j >= 0; j-- {
+		if nums[j] > nums[i] {
+			nums[j], nums[i] = nums[i], nums[j]
+			break
+		}
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(nums []int) {
+	left, right := 0, len(nums)-1
+	for left < right {
+		nums[left], nums[right] = nums[right], nums[left]
+		left++
+		right--
+	}
 }
--- 2021/05/25/91DecodeWays/main.go
+++ 2021/05/25/91DecodeWays/main.go
@@ -1,31 +1,60 @@
 package main
 
+import (
+	"strconv"
+)
+
 func numDecodings(s string) int {
-	if s[0] == '0' {
+	if len(s) == 0 {
 		return 0
 	}
-	start, result := 0, 0
-	dfs(s, start, &result)
+	dp := make([]int, len(s)+1)
+	dp[0] = 1
+	if s[0] != '0' {
+		dp[1] = 1
+	}
+
+	for i := 2; i <= len(s); i++ {
+		lastNum, _ := strconv.Atoi(s[i-1 : i])
+		if lastNum >= 1 && lastNum <= 9 {
+			dp[i] += dp[i-1]
+		}
+		lastNum, _ = strconv.Atoi(s[i-2 : i])
+		if lastNum >= 10 && lastNum <= 26 {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[len(s)]
+}
+
+// 暴力递归超时
+func numDecodings1(s string) int {
+	result := 0
+	helper(s, &result)
 	return result
 }
 
-func dfs(s string, start int, result *int) {
-	if start == len(s) {
+func helper(s string, result *int) {
+	if len(s) == 0 {
 		*result++
 		return
 	}
+	if s[0] == '0' {
+		return
+	}
+	if s[0] == '1' {
+		helper(s[1:], result)
+		if len(s) > 1 {
+			helper(s[2:], result)
+		}
+	} else if s[0] == '2' {
+		helper(s[1:], result)
+		if len(s) > 1 && s[1] <= '6' {
+			helper(s[2:], result)
+		}
+	} else {
+		helper(s[1:], result)
+	}
 
-	if s[start] == '1' {
-		if start+2 <= len(s) {
-			dfs(s, start+2, result)
-		}
-	}
-	if s[start] == '2' {
-		if start+2 <= len(s) && s[start+1] <= '6' {
-			dfs(s, start+2, result)
-		}
-	}
-	if s[start+1] != '0' {
-		dfs(s, start+1, result)
-	}
 }
