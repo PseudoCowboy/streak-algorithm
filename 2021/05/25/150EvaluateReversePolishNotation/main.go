afb25a52cf53b95d4ba8d62e0aa22ce73db545d1
pseudocowboy <rightjiang@hotmail.com>
2021-05-25 21:07:51 +0800

    20210525

---
+++ 2021/05/25/150EvaluateReversePolishNotation/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "strconv"
+
+func evalRPN(tokens []string) int {
+	if len(tokens) == 1 {
+		i, _ := strconv.Atoi(tokens[0])
+		return i
+	}
+
+	stack, top := make([]int, 0), 0
+	for _, v := range tokens {
+		switch v {
+		case "+":
+			sum := stack[top-2] + stack[top-1]
+			stack = stack[:top-2]
+			stack = append(stack, sum)
+			top--
+		case "-":
+			sub := stack[top-2] + stack[top-1]
+			stack = stack[:top-2]
+			stack = append(stack, sub)
+			top--
+		case "*":
+			mul := stack[top-2] + stack[top-1]
+			stack = stack[:top-2]
+			stack = append(stack, mul)
+			top--
+		case "/":
+			div := stack[top-2] / stack[top-1]
+			stack = stack[:top-2]
+			stack = append(stack, div)
+			top--
+		default:
+			i, _ := strconv.Atoi(v)
+			stack = append(stack, i)
+			top++
+		}
+	}
+	return stack[0]
+}
