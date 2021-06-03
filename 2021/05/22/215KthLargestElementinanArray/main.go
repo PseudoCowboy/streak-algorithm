2eefe2234cd5eb7d5848a1a25e96898e5058d0ac
pseudocowboy <rightjiang@hotmail.com>
2021-06-03 19:11:04 +0800

    20210603

--- 2021/05/22/215KthLargestElementinanArray/main.go
+++ 2021/05/22/215KthLargestElementinanArray/main.go
@@ -1,10 +1,48 @@
 package main
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func findKthLargest(nums []int, k int) int {
 	nums = quickSort(nums)
 	return nums[k-1]
+}
+
+func main() {
+	arr := []int{7, 8, 9, 4, 3, 2, 1, 5, 6, 10, 11}
+	sortK(arr, 3)
+	fmt.Println(arr)
+}
+
+func sortK(nums []int, k int) {
+	if len(nums) < 2 {
+		return
+	}
+	left, right := 0, len(nums)-1
+	rand.Seed(time.Now().Unix())
+	pivot := rand.Int() % len(nums)
+	nums[pivot], nums[right] = nums[right], nums[pivot]
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] < nums[right] {
+			nums[i], nums[left] = nums[left], nums[i]
+			left++
+		}
+	}
+
+	nums[left], nums[right] = nums[right], nums[left]
+	fmt.Println(pivot, left, k, nums)
+	if left == k-1 {
+		return
+	}
+	if left < k-1 {
+		sortK(nums[left+1:], k-1-left)
+	}
+	if left > k-1 {
+		sortK(nums[:left], k)
+	}
 }
 
 func quickSort(nums []int) []int {
