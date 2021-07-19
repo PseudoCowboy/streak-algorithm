4f8e0db9831bb5bb94656a6a14f9c9d3d21116a6
pseudocowboy <rightjiang@hotmail.com>
2021-07-20 00:24:42 +0800

    20210719

--- 2021/07/19/45JumpGameII/main.go
+++ 2021/07/19/45JumpGameII/main.go
@@ -1,5 +1,22 @@
 package main
 
 func jump(nums []int) int {
+	if len(nums) == 1 {
+		return 0
+	}
 
+	needChoose, canReach, step := 0, 0, 0
+	for i, x := range nums {
+		if i+x > canReach {
+			canReach = i + x
+			if canReach >= len(nums)-1 {
+				return step + 1
+			}
+		}
+		if i == needChoose {
+			needChoose = canReach
+			step++
+		}
+	}
+	return step
 }
